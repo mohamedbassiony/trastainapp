63008fd4451bca39da5c6bcba8dad242010109c3
mohamedbassiony <mohamedbassiony333@gmail.com>
2024-08-04 10:33:22 +0300

    update main with calling the base file to run test fast api

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from routes import base
+
 app = FastAPI()
 
-
-@app.get("/")
-def welcome():
-    return {
-        "message": "Hello World!"
-    }
+app.include_router(base.base_router)
