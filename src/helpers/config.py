03ab54844c80f02655ec236462ae3dc2add791b9
mohamedbassiony <mohamedbassiony333@gmail.com>
2024-08-21 19:16:41 +0300

    Update config.py with firebase path type

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -8,7 +8,9 @@ class Settings(BaseSettings):
 
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
-    FILE_DEFAULT_CHUNK_SIZE: int
+    FILE_DEFAULT_CHUNK_SIZE: int = 1024 * 1024
+
+    FIREBASE_CREDENTIALS_PATH: str
 
     class Config:
         env_file = ".env"
